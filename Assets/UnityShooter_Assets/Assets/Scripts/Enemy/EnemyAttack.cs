c87af5b0ee7547982d2e9f31971b6809deb180d4
Dylan Dixon <dylan.dixon@g.austincc.edu>
2025-07-12 14:34:54 -0500

    Made the "GameObject player" in EnemyAttack and "Transform playerPosition" both public statics for EnemyManager to use "GameObject.FindGameObjectWithTag("Player")". Also added public and private keywords to variables.

--- Assets/UnityShooter_Assets/Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/UnityShooter_Assets/Assets/Scripts/Enemy/EnemyAttack.cs
@@ -4,24 +4,25 @@ public class EnemyAttack : MonoBehaviour
 {
     public float timeBetweenAttacks = 0.5f;
     public int attackDamage = 10;
+    public static GameObject player; //Also used in EnemyManager
 
-    Animator anim;
-    GameObject player;
-    PlayerHealth playerHealth;
-    EnemyHealth enemyHealth;
-    bool playerInRange;
-    float timer;
+    private Animator anim;
+    private PlayerHealth playerHealth;
+    private EnemyHealth enemyHealth;
+    private bool playerInRange;
+    private float timer;
 
     private void Awake()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
         playerHealth = player.GetComponent<PlayerHealth>();
         enemyHealth = GetComponent<EnemyHealth>();
         anim = GetComponent<Animator>();
     }
 
-    //When enemy gets close enough to player
-    void OnTriggerEnter(Collider other)
+    /// <summary>
+    /// When enemy gets close enough to player
+    /// </summary>
+    private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == player)
         {
@@ -29,7 +30,9 @@ public class EnemyAttack : MonoBehaviour
         }
     }
 
-    //When player steps away from the enemy
+    /// <summary>
+    /// When player steps away from the enemy
+    /// </summary>
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject == player) 
@@ -38,8 +41,8 @@ public class EnemyAttack : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    private void Update()
     {
         timer += Time.deltaTime;
         if (timer >= timeBetweenAttacks && playerInRange && enemyHealth.currentHealth > 0)
@@ -53,8 +56,10 @@ public class EnemyAttack : MonoBehaviour
         }
     }
 
-    //Enemy hurts player
-    void Attack()
+    /// <summary>
+    /// Enemy hurts player
+    /// </summary>
+    private void Attack()
     {
         timer = 0f;
         if (playerHealth.currentHealth > 0)
--- Assets/UnityShooter_Assets/Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/UnityShooter_Assets/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -3,27 +3,28 @@ using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
-    
-    Transform player;
-    PlayerHealth playerHealth;
-    EnemyHealth enemyHealth;
-    NavMeshAgent nav;
+    private PlayerHealth playerHealth;
+    private EnemyHealth enemyHealth;
+    private NavMeshAgent nav;
+
+    public static Transform playerPosition; //Also used in EnemyManager
 
     private void Awake()
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-        playerHealth = player.GetComponent<PlayerHealth>();
+        playerHealth = playerPosition.GetComponent<PlayerHealth>();
         enemyHealth = GetComponent<EnemyHealth>();
         nav = GetComponent<NavMeshAgent>();
     }
 
-    // The enemy will walk towards the player as long as the player (or the enemy) is not dead.
+    /// <summary>
+    /// The enemy will walk towards the player as long as the player (or the enemy) is not dead.
+    /// </summary>
     private void Update()
     {
         
         if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
         {
-            nav.SetDestination(player.position);
+            nav.SetDestination(playerPosition.position);
         }
         else
         {
